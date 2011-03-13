1a4700d3d3d294232a88126532ebe6b29abd7e12
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-03-13 16:05:46 +0100

    Implemented nearest neighbor heuristic for TSP

--- robot_tour_optimization.js
+++ robot_tour_optimization.js
@@ -8,31 +8,53 @@ var point = function (x, y) {
         "y": y,
 
         distanceTo: function (otherPoint) {
-            return Math.sqrt(square(x - otherPoint.x) + square(y - otherPoint.y));
+            return Math.sqrt(square(this.x - otherPoint.x) + square(this.y - otherPoint.y));
         }
     };
 };
 
+var copyArray = function (a) {
+    var ret = [];
+    
+    for (var i = 0; i < a.length; i++) {
+        ret[i] = a[i];
+    }
+    
+    return ret;
+};
+
+var arrayWithout = function (a, index) {
+    return a.filter(function (e, eIndex) {
+        return (eIndex !== index);
+    });
+};
+
+var findNearestPointIndex = function (points, toPoint) {
+    var d, result;
+    
+    for (var i = 0; i < points.length; i++) {
+        if ((d === undefined) || (d > points[i].distanceTo(toPoint))) {
+            d = points[i].distanceTo(toPoint);
+            result = i;
+        }
+    }
+    
+    return result;
+};
+
 var nearestNeighbourTour = function (points) {
+    var pool = copyArray(points);
+    var result = [pool.shift()];
+    
+    while (pool.length > 0) {
+        var nextPoolIndex = findNearestPointIndex(pool, result[result.length - 1]);
+        result.push(pool[nextPoolIndex]);
+        pool = arrayWithout(pool, nextPoolIndex);
+    }
+    
+    return result;
+};
+
+var closestPairTour = function (points) {
     
 };
-
-var circle = function () {
-    return [
-        point(0, 5),
-        point(5, 0),
-        point(0, -5),
-        point(-5, 0),
-        point(-5, 0),
-        point(3, 4),
-        point(4, 3),
-        point(4, -3),
-        point(3, -4),
-        point(-3, -4),
-        point(-4, -3),
-        point(-4, 3),
-        point(-3, 4)
-    ];
-};
-
-print(nearestNeighbourTour(circle()));
