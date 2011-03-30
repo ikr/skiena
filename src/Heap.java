35afa6deef9c529a8202f3a7a07dd67c120bd3df
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-03-30 18:40:12 +0200

    Heap.insert()

--- src/Heap.java
+++ src/Heap.java
@@ -1,6 +1,48 @@
+/**
+ * An integer min-heap. See http://en.wikipedia.org/wiki/Binary_heap 
+ */
 public class Heap {
+    private int[] queue = new int[10000];
+    private int queueLength = 0;
+    
     public int[] toArray() {
-        return new int[0];
+        int[] result = new int[queueLength];
+        
+        for (int i = 0; i < queueLength; ++i) {
+            result[i] = queue[i];
+        }
+        
+        return result;
     }
 
+    public static int parentIndex(int index) {
+        return (index > 0)? ((index - 1) / 2) : -1;
+    }
+
+    public static int firstChildIndex(int index) {
+        return index * 2 + 1;
+    }
+
+    public void insert(int x) {
+        queue[queueLength] = x;
+        ++queueLength;
+        bubbleUp(queueLength - 1);
+    }
+    
+//--------------------------------------------------------------------------------------------------    
+    
+    private void bubbleUp(int index) {
+        if (-1 == parentIndex(index)) return;
+        
+        if (queue[index] < queue[parentIndex(index)]) {
+            swap(index, parentIndex(index));
+            bubbleUp(parentIndex(index));
+        }
+    }
+    
+    private void swap(int indexA, int indexB) {
+        int originalA = queue[indexA];
+        queue[indexA] = queue[indexB];
+        queue[indexB] = originalA;
+    }
 }
--- src/HeapTest.java
+++ src/HeapTest.java
@@ -1,6 +1,49 @@
 import junit.framework.TestCase;
 
 public class HeapTest extends TestCase {
+    public void testParentIndexOfZeroReturnsAnInvalidValue() {
+        assertEquals(-1, Heap.parentIndex(0));
+    }
+
+    public void testParentIndexOfOneIsZero() {
+        assertEquals(0, Heap.parentIndex(1));
+    }
+
+    public void testParentIndexOfTwoIsZero() {
+        assertEquals(0, Heap.parentIndex(2));
+    }
+    
+    public void testParentIndexOfThreeIsOne() {
+        assertEquals(1, Heap.parentIndex(3));
+    }
+    
+    public void testParentIndexOfSixIsTwo() {
+        assertEquals(1, Heap.parentIndex(3));
+    }
+        
+    public void testParentIndexOfEightIsThree() {
+        assertEquals(3, Heap.parentIndex(8));
+    }
+
+//--------------------------------------------------------------------------------------------------
+
+    public void testFirstChildIndexOfZeroIsOne() {
+        assertEquals(1, Heap.firstChildIndex(0));
+    }
+    
+    public void testFirstChildIndexOfOneIsThree() {
+        assertEquals(3, Heap.firstChildIndex(1));
+    }
+    
+    public void testFirstChildIndexOfTwoIsFive() {
+        assertEquals(5, Heap.firstChildIndex(2));
+    }
+    
+    public void testFirstChildIndexOfTheeIsSeven() {
+        assertEquals(7, Heap.firstChildIndex(3));
+    }
+    
+//--------------------------------------------------------------------------------------------------
     public void testJavaArrayInitialization() {
         int[] a = new int[3];
         assertEquals(0, a[0]);
@@ -11,11 +54,42 @@ public class HeapTest extends TestCase {
         assertArraysEqual(new int[0], h().toArray());
     }
     
-    private void assertArraysEqual(int[] a, int[] b) {
-        assertEquals("Must have same length", a.length, b.length);
+    public void testInsertsFirstElement() {
+        Heap h = h();
+        h.insert(42);
         
-        for (int i  = 0; i < a.length; ++i) {
-            assertEquals("Same values at index " + Integer.toString(i) + "expected", a[i], b[i]);
+        int[] expected = {42};
+        
+        assertArraysEqual(expected, h.toArray());
+    }
+    
+    public void testInsertsMonotonicSecondElement() {
+        Heap h = h();
+        h.insert(1492);
+        h.insert(2001);
+        
+        int[] expected = {1492, 2001};
+        
+        assertArraysEqual(expected, h.toArray());
+    }
+    
+    public void testInsertsNonMonotonicSecondElement() {
+        Heap h = h();
+        h.insert(2001);
+        h.insert(1492);
+        
+        int[] expected = {1492, 2001};
+        
+        assertArraysEqual(expected, h.toArray());
+    }
+    
+//--------------------------------------------------------------------------------------------------
+    
+    private void assertArraysEqual(int[] expected, int[] actual) {
+        assertEquals("Must have same length", expected.length, actual.length);
+        
+        for (int i  = 0; i < expected.length; ++i) {
+            assertEquals("Values at index " + Integer.toString(i), expected[i], actual[i]);
         }
     }
     
